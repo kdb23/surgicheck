5911f3dc750378b4edcd45a0e4234d0fe24cea55
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-24 16:30:40 -0400

    inital user login

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from config import app, api
 from models import User
-from flask import make_response
+from flask import make_response, session, request
 from flask_restful import Resource
 
 class Home(Resource):
@@ -8,6 +8,20 @@ class Home(Resource):
         return 'Hi'
     
 api.add_resource(Home, '/')
+
+class Login(Resource):
+    def post(self):
+
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username)
+
+        password = request.get_json()['password']
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
