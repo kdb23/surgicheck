db7864a0d97999e9faeda0e6d555acb930676035
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-04-24 15:21:22 -0400

    addition of flask_restful, metadata, cors, and sqlalchemy

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from config import app, api
+from flask import make_response
+from flask_restful import Resource
+
+class Home(Resource):
+    def get(self):
+        return 'Hi'
+    
+api.add_resource(Home, '/')
+
+if __name__ == '__main__':
+    app.run(port=5555)
+    
--- server/config.py
+++ server/config.py
@@ -1,1 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_cors import CORS
+from sqlalchemy import MetaData
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///models.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+md = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata = md)
+migrate = Migrate( app, db)
+
+db.init_app(app)
+api = Api(app)
+
+CORS(app)
