4fc76ca2f695ead3c0ccbfb85a25f3348aa91cda
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-05-02 16:48:47 -0400

    table migrations for username and password

--- server/models.py
+++ server/models.py
@@ -10,7 +10,7 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
+    username = db.Column(db.String, nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
 
     @hybrid_property
